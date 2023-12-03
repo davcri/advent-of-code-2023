37804a6fe7af78c10e6f5a748dbf4570de5243ff
Davide Cristini <davcri91@gmail.com>
2023-12-03 17:48:49 +0100

    day 03: puzzle 2

--- day03/puzzle.ts
+++ day03/puzzle.ts
@@ -12,6 +12,16 @@ class CellCoordinates {
     this.col = col;
   }
 
+  isIn(rows: number, cols: number): boolean {
+    return this.row >= 0 && this.row < rows && this.col >= 0 && this.col < cols;
+  }
+
+  add(other: CellCoordinates): CellCoordinates {
+    this.row += other.row;
+    this.col += other.col;
+    return this;
+  }
+
   toString(): string {
     return `(${this.row}, ${this.col})`;
   }
@@ -26,16 +36,8 @@ class EngineSchematic {
   constructor(blueprint: string) {
     let lines = blueprint.split("\n");
     lines = lines.filter((l) => l !== "");
-
     this.schematicRows = lines.length;
     this.schematicCols = lines[0].length;
-
-    console.log(
-      "🚀 ~ schematic Rows Cols:",
-      this.schematicRows,
-      this.schematicCols
-    );
-
     let row: string[] = [];
 
     lines.forEach((l, idx) => {
@@ -43,11 +45,7 @@ class EngineSchematic {
       for (let i = 0; i < l.length; i++) {
         const cellValue = l[i];
         row.push(cellValue);
-        if (
-          !this.symbols.has(cellValue) &&
-          cellValue !== "." &&
-          !isNumber(cellValue)
-        ) {
+        if (!this.symbols.has(cellValue) && cellValue !== "." && !isNumber(cellValue)) {
           this.symbols.add(cellValue);
         }
       }
@@ -55,14 +53,15 @@ class EngineSchematic {
     });
 
     console.log(this.symbols);
+    console.log();
   }
 
   isSymbol(c: string) {
     return this.symbols.has(c);
   }
 
-  _findPartNumberStartCoordinate(cc: CellCoordinates): CellCoordinates {
-    let sc = new CellCoordinates(cc.row, cc.col); // start coordinate
+  _findPartNumberStartCoordinate(cc: Readonly<CellCoordinates>): CellCoordinates {
+    const sc = new CellCoordinates(cc.row, cc.col); // start coordinate
     while (sc.col > 0 && isNumber(this.schematics[sc.row][sc.col - 1])) {
       sc.col--;
     }
@@ -86,143 +85,124 @@ class EngineSchematic {
     return num;
   }
 
+  isGear(cell: string) {
+    return cell === "*";
+  }
+
   getPartNumbers() {
     const { schematicCols, schematicRows, schematics } = this;
     const partNumbers = new Map<string, number>(); // key: cell coordinates as string , value: part number
-    const checkAdjacentCells = (cc: CellCoordinates) => {
-      // check east
-      if (cc.col + 1 < schematicCols) {
-        const cellValue = schematics[cc.row][cc.col + 1];
-        if (isNumber(cellValue)) {
-          const startIndex = this._findPartNumberStartCoordinate({
-            row: cc.row,
-            col: cc.col + 1,
-          });
-          if (partNumbers.has(startIndex.toString())) {
-          } else {
-            const partNumber = this._readPartNumber(startIndex);
-            partNumbers.set(startIndex.toString(), partNumber);
-          }
-        }
-      }
-      // check south east
-      if (cc.col + 1 < schematicCols && cc.row + 1 < schematicRows) {
-        const cellValue = schematics[cc.row + 1][cc.col + 1];
-        if (isNumber(cellValue)) {
-          const startIndex = this._findPartNumberStartCoordinate({
-            row: cc.row + 1,
-            col: cc.col + 1,
-          });
-          if (partNumbers.has(startIndex.toString())) {
-          } else {
-            const partNumber = this._readPartNumber(startIndex);
-            partNumbers.set(startIndex.toString(), partNumber);
-          }
-        }
-      }
-      // check south
-      if (cc.row + 1 < schematicRows) {
-        const cellValue = schematics[cc.row + 1][cc.col];
-        if (isNumber(cellValue)) {
-          const startIndex = this._findPartNumberStartCoordinate({
-            row: cc.row + 1,
-            col: cc.col,
-          });
-          if (partNumbers.has(startIndex.toString())) {
-          } else {
-            const partNumber = this._readPartNumber(startIndex);
-            partNumbers.set(startIndex.toString(), partNumber);
-          }
-        }
-      }
-      // check south west
-      if (cc.row === 5) debugger;
-      if (cc.col - 1 >= 0 && cc.row + 1 < schematicRows) {
-        const cellValue = schematics[cc.row + 1][cc.col - 1];
-        if (isNumber(cellValue)) {
-          const startIndex = this._findPartNumberStartCoordinate({
-            row: cc.row + 1,
-            col: cc.col - 1,
-          });
-          if (partNumbers.has(startIndex.toString())) {
-          } else {
-            const partNumber = this._readPartNumber(startIndex);
-            partNumbers.set(startIndex.toString(), partNumber);
-          }
-        }
-      }
-      // check west
-      if (cc.col - 1 >= 0) {
-        const cellValue = schematics[cc.row][cc.col - 1];
-        if (isNumber(cellValue)) {
-          const startIndex = this._findPartNumberStartCoordinate({
-            row: cc.row,
-            col: cc.col - 1,
-          });
-          if (partNumbers.has(startIndex.toString())) {
-          } else {
-            const partNumber = this._readPartNumber(startIndex);
-            partNumbers.set(startIndex.toString(), partNumber);
-          }
-        }
-      }
-      // check north west
-      if (cc.col - 1 >= 0 && cc.row - 1 >= 0) {
-        const cellValue = schematics[cc.row - 1][cc.col - 1];
-        if (isNumber(cellValue)) {
-          const startIndex = this._findPartNumberStartCoordinate({
-            row: cc.row - 1,
-            col: cc.col - 1,
-          });
-          if (partNumbers.has(startIndex.toString())) {
-          } else {
-            const partNumber = this._readPartNumber(startIndex);
-            partNumbers.set(startIndex.toString(), partNumber);
-          }
-        }
-      }
-      // check north
-      if (cc.row - 1 >= 0) {
-        const cellValue = schematics[cc.row - 1][cc.col];
-        if (isNumber(cellValue)) {
-          const startIndex = this._findPartNumberStartCoordinate({
-            row: cc.row - 1,
-            col: cc.col,
-          });
-          if (partNumbers.has(startIndex.toString())) {
-          } else {
-            const partNumber = this._readPartNumber(startIndex);
-            partNumbers.set(startIndex.toString(), partNumber);
-          }
-        }
-      }
-      // check north east
-      if (cc.col + 1 < schematicCols && cc.row - 1 >= 0) {
-        const cellValue = schematics[cc.row - 1][cc.col + 1];
-        if (isNumber(cellValue)) {
-          const startIndex = this._findPartNumberStartCoordinate({
-            row: cc.row - 1,
-            col: cc.col + 1,
-          });
-          if (partNumbers.has(startIndex.toString())) {
-          } else {
-            const partNumber = this._readPartNumber(startIndex);
-            partNumbers.set(startIndex.toString(), partNumber);
-          }
-        }
-      }
-    };
     for (let row = 0; row < schematicRows; row++) {
       let rowString = "";
       for (let col = 0; col < schematicCols; col++) {
         const cell = schematics[row][col];
         if (this.isSymbol(cell)) {
-          checkAdjacentCells({ row, col });
+          this.checkAdjacentCellsForPartNumbers(
+            new CellCoordinates(row, col),
+            partNumbers
+          );
         }
         rowString += `${cell}`;
       }
     }
     return partNumbers;
+  }
+
+  getGearRatios() {
+    const { schematicCols, schematicRows, schematics } = this;
+    const gearRatios = new Map<string, number>(); // key: cell coordinates as string , value: gear ratio
+    for (let row = 0; row < schematicRows; row++) {
+      let rowString = "";
+      for (let col = 0; col < schematicCols; col++) {
+        const cell = schematics[row][col];
+        if (this.isGear(cell)) {
+          this.checkAdjacentCellsForGears(new CellCoordinates(row, col), gearRatios);
+        }
+        rowString += `${cell}`;
+      }
+    }
+    return gearRatios;
+  }
+
+  checkAdjacentCellsForGears(
+    cc: Readonly<CellCoordinates>,
+    gearRatios: Map<string, number>
+  ) {
+    const { schematicCols, schematicRows, schematics } = this;
+
+    const dirs = [
+      new CellCoordinates(0, 1), // east
+      new CellCoordinates(1, 1), // south east
+      new CellCoordinates(1, 0), // south
+      new CellCoordinates(1, -1), // south west
+      new CellCoordinates(0, -1), // west
+      new CellCoordinates(-1, -1), // north west
+      new CellCoordinates(-1, 0), // north
+      new CellCoordinates(-1, 1), // north east
+    ];
+
+    const localPartNumbers = new Map<string, number>();
+
+    for (let i = 0; i < dirs.length; i++) {
+      const dir = dirs[i];
+      const newCoordinate = new CellCoordinates(cc.row + dir.row, cc.col + dir.col);
+      if (!newCoordinate.isIn(schematicRows, schematicCols)) {
+        console.log("skipping ", newCoordinate.toString());
+        continue;
+      }
+
+      const cellValue = schematics[newCoordinate.row][newCoordinate.col];
+      if (isNumber(cellValue)) {
+        const startIndex = this._findPartNumberStartCoordinate(newCoordinate);
+
+        if (!localPartNumbers.has(startIndex.toString())) {
+          const partNumber = this._readPartNumber(startIndex);
+          localPartNumbers.set(startIndex.toString(), partNumber);
+        }
+      }
+    }
+
+    if (localPartNumbers.size === 2) {
+      const partNumbers = Array.from(localPartNumbers.values());
+      const gearRatio = partNumbers[0] * partNumbers[1];
+      gearRatios.set(cc.toString(), gearRatio);
+    }
+  }
+
+  checkAdjacentCellsForPartNumbers(
+    cc: Readonly<CellCoordinates>,
+    partNumbers: Map<string, number>
+  ) {
+    const { schematicCols, schematicRows, schematics } = this;
+
+    const dirs = [
+      new CellCoordinates(0, 1), // east
+      new CellCoordinates(1, 1), // south east
+      new CellCoordinates(1, 0), // south
+      new CellCoordinates(1, -1), // south west
+      new CellCoordinates(0, -1), // west
+      new CellCoordinates(-1, -1), // north west
+      new CellCoordinates(-1, 0), // north
+      new CellCoordinates(-1, 1), // north east
+    ];
+
+    for (let i = 0; i < dirs.length; i++) {
+      const dir = dirs[i];
+      const newCoordinate = new CellCoordinates(cc.row + dir.row, cc.col + dir.col);
+      if (!newCoordinate.isIn(schematicRows, schematicCols)) {
+        console.log("skipping ", newCoordinate.toString());
+        continue;
+      }
+
+      const cellValue = schematics[newCoordinate.row][newCoordinate.col];
+      if (isNumber(cellValue)) {
+        const startIndex = this._findPartNumberStartCoordinate(newCoordinate);
+        if (!partNumbers.has(startIndex.toString())) {
+          const partNumber = this._readPartNumber(startIndex);
+          partNumbers.set(startIndex.toString(), partNumber);
+        }
+      }
+    }
   }
 
   printSchematics() {
@@ -253,13 +233,22 @@ helper.benchmark(async () => {
 
 async function puzzle1(puzzleInput: string) {
   const engine = new EngineSchematic(puzzleInput);
-  // engine.printSchematics();
   const partNumbers = engine.getPartNumbers();
   let sum = 0;
   partNumbers.forEach((partNumber) => {
     sum += partNumber;
   });
-  console.log("🚀 ~ sum:", sum);
+  console.log("🚀 ~ part numbers sum:", sum);
 }
 
-async function puzzle2(input: string) {}
+async function puzzle2(input: string) {
+  const engine = new EngineSchematic(puzzleInput);
+  const gearRatios = engine.getGearRatios();
+  // console.log("🚀 ~ gearRatios:", gearRatios);
+
+  let sum = 0;
+  gearRatios.forEach((gr) => {
+    sum += gr;
+  });
+  console.log("🚀 ~ gear ratios sum:", sum);
+}
